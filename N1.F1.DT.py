2033bca9c76485342b1ebc1c7b5f502585a6b3ae
Lulusidev <lulusiprincipal@gmail.com>
2021-07-22 11:50:51 -0300

    first commit

---
+++ N1.F1.DT.py
@@ -0,0 +1,4 @@
+A = int(input("Quanto dinheiro os piratas encontraram? "))
+N = int(input("Qual o numero de marinheiros a bordo ? "))
+M = A / (N + 2)
+print("o Capitão Olho Roxo recebeu {}".format(int(2 * M)))
---
+++ N1.F1.EM.py
@@ -0,0 +1,14 @@
+M = input("Mensagem : ")
+L = M.count(":-)")
+C = M.count(":-(")
+
+if L > C :
+	print("divertido")
+
+elif L == C :
+	print("neutro")
+
+elif L < C :
+	print("chateado")
+
+
