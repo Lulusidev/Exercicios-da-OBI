df697ac341ec6d1ac284eae56c2930ea1209a111
Lulusidev <lulusiprincipal@gmail.com>
2021-07-25 23:01:48 -0300

    Fase 2

--- N1.F1.DT.py
+++
@@ -1,4 +0,0 @@
-A = int(input("Quanto dinheiro os piratas encontraram? "))
-N = int(input("Qual o numero de marinheiros a bordo ? "))
-M = A / (N + 2)
-print("o Capitão Olho Roxo recebeu {}".format(int(2 * M)))
